9ebd074848b1cbf208ca2da9590bb61d88d75f66
gerumpy <samuel.wilton.03@gmail.com>
2024-02-21 15:18:09 -0500

    Fixes

--- plate.gd
+++ plate.gd
@@ -30,7 +30,7 @@ func _process(_delta: float) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body is Waiter:
 		body = body as Waiter
-		if !available and body.food_carried.size() <= body.carry_capacity:
+		if !available and body.food_carried.size() < body.carry_capacity:
 			available = true
 			hide()
 			get_node("%Food/"+order).hide() 
